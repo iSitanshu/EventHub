bfc9c5ab5ef4405d2637ad2afd723efa2c2b1804
iSitanshu <Sitanshumishra18@gmail.com>
2024-07-27 05:10:48 +0530

    PopularEvent

--- Front/src/components/PopularEvents/PopularEvents.jsx
+++ Front/src/components/PopularEvents/PopularEvents.jsx
@@ -8,9 +8,10 @@ const PopularEvents = () => {
     <p className='popularevent-heading'>
       <span>Popular Events</span> in New Delhi</p> 
     <div className='popularevent-button'>
-        <button>Standup</button>
         <button>Movies</button>
+        <button>Standup</button>
         <button>Plays</button>
+        <button>Events</button>
         <button>Other Events</button>
     </div>
 </div>
